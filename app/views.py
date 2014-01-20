ab912da01293e810720c45e8c1000ecbb8f98df4
Christopher Bischak <cab3wz@virginia.edu>
2014-01-20 13:09:29 -0500

    Better Looking
    Made it slightly better looking

--- app/views.py
+++ app/views.py
@@ -4,7 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # fake user
-    return render_template("index.html",
-        title = 'Home',
-        user = user)
+    return render_template("index.html", 
+        title = 'Home')
