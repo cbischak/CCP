acffec91bcff803e9b4ff8204bbe7780baec34b6
Christopher Bischak <cab3wz@virginia.edu>
2014-01-21 12:51:30 -0500

    Dynamic Linking
    Dynamic Linking to everything but blog and donate

--- app/views.py
+++ app/views.py
@@ -6,3 +6,27 @@ from app import app
 def index():
     return render_template("index.html", 
         title = 'Home')
+@app.route('/home')
+def home():
+    return render_template("index.html",
+        title = "Home")
+@app.route('/about')
+def about():
+    return render_template("about.html", 
+        title = 'about')
+@app.route('/contact')
+def contact():
+    return render_template("contact.html",
+        title = "contact")
+@app.route('/photos')
+def photos():
+    return render_template("photos.html",
+        title = "photos")
+@app.route('/season')
+def season():
+    return render_template("season.html",
+        title = "season")
+@app.route('/class')
+def classes():
+    return render_template("class.html",
+        title = "class")
